befe2325be990d162ea07658df18e249ad2c7684
Ilija Vrajich <vrajich1@gmail.com>
2019-06-06 09:07:19 -0600

    Code Updated Correctly

--- src/index.js
+++ src/index.js
@@ -1,36 +1,47 @@
 import Senators from './data/senators'
 
 export const republicans = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator => senator.party === 'Republican')
 }
 
 export const democrats = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator => senator.party === 'Democrat')
 }
 
 export const independents = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator => senator.party === 'Independent')
 }
 
 export const males = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator => senator.person.gender === 'male')
 }
 
 export const females = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator => senator.person.gender === 'female')
 }
 
 export const byState = (state = 'UT') => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator => senator.state === 'UT')
 }
 
-export const mapping = () => {
-  return REPLACE_ME_WITH_CODE
+  export const mapping = () => {
+     return Senators.map (senator => {
+      return {
+              firstName: senator.person.firstname,
+              lastName: senator.person.lastname,
+              party: senator.party,
+              gender: senator.person.gender
+   
+    }})
 }
 
 export const birthplaceSeniorSenator = () => {
-  return REPLACE_ME_WITH_CODE
+  
+  const senatorState = Senators.filter(senator => senator.state === 'CA' && senator.senator_rank === 'senior')
+  const senatorSenior = senatorState.reduce(senior => senior)
+  return senatorSenior
+  
 }
 
 
-const REPLACE_ME_WITH_CODE = false
+
